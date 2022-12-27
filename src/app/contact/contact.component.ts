0a3eb849602655886748bb377abb2e0365aeffd6
subbu999 <b.subbu504@gmail.com>
2022-12-28 01:26:53 +0530

    contact form added

--- src/app/contact/contact.component.ts
+++ src/app/contact/contact.component.ts
@@ -1,4 +1,6 @@
+import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-contact',
@@ -6,10 +8,36 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./contact.component.css']
 })
 export class ContactComponent implements OnInit {
-
-  constructor() { }
+  spinModalDisplay = "none";
+  thankModalDisplay = "none";
+  constructor(private http: HttpClient) { }
 
   ngOnInit(): void {
+  }
+  onFormSubmit(form: NgForm) {
+    this.spinModalDisplay = "block";
+    let data = {
+      name: form.controls.name.value,
+      email: form.controls.email.value,
+      message: form.controls.message.value,
+      _template: "table",
+      _captcha: "false"
+    }
+    this.http.post("https://formsubmit.co/b.subbu504@gmail.com", data).subscribe(
+      (res) => { console.log(res) },
+      (err) => {
+        this.spinModalDisplay = "none";
+        this.thankModalDisplay = "block";
+        setTimeout(function () {
+          this.thankModalDisplay = "block";
+        }, 2000);
+      },
+      () => { console.log('completed') }
+    );
+  }
+  reload() {
+    this.thankModalDisplay = 'none';
+    window.location.reload();
   }
 
 }
